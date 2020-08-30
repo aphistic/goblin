e73c54034cdcb7ca1cead59f1a56cbb30af36156
Kristin Davidson <k@kxd.dev>
2020-08-30 01:00:02 +0000

    Add docs to internal logging package

--- internal/logging/logger.go
+++ internal/logging/logger.go
@@ -1,31 +1,37 @@
+// Package logging is the logger the Goblin binary uses internally.
 package logging
 
 import "fmt"
 
+// Logger is an interface representint a logger.
 type Logger interface {
 	Printf(string, ...interface{})
 }
 
-type NilLogger struct {}
+// NilLogger is a Logger that does nothing.
+type NilLogger struct{}
 
 var _ Logger = &NilLogger{}
 
+// NewNilLogger creates a new nil logger.
 func NewNilLogger() *NilLogger {
 	return &NilLogger{}
 }
 
-func (l *NilLogger) Printf(format string, args ...interface{}) {
+// Printf does nothing.
+func (l *NilLogger) Printf(format string, args ...interface{}) {}
 
-}
-
-type PrintfLogger struct {}
+// PrintfLogger uses fmt.Printf for logging.
+type PrintfLogger struct{}
 
 var _ Logger = &PrintfLogger{}
 
+// NewPrintfLogger creates a new fmt.Printf logger.
 func NewPrintfLogger() *PrintfLogger {
 	return &PrintfLogger{}
 }
 
+// Printf uses fmt.Printf to log messages.
 func (l *PrintfLogger) Printf(format string, args ...interface{}) {
 	fmt.Printf(format, args...)
 }
